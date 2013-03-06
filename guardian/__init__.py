7287ab902954196bf772d63e52c220ea61d83f1b
Stefano DAmico <stefano@seatme.com>
2013-03-06 15:45:13 -0800

    Fix object perm admin username limiting

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -1,7 +1,7 @@
 """
 Implementation of per object permissions for Django 1.2.
 """
-VERSION = (1, 0, 3, 'sm0')
+VERSION = (1, 0, 3, 'sm1')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
